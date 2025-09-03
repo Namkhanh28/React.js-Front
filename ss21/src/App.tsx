70e2434ff6d56283783b6c3e3549cb4632b64899
Nam Khanh <nguyendanhnamkhanh06@gmail.com>
2025-09-04 01:36:41 +0700

    BTvn

---
+++ ss21/src/App.tsx
@@ -0,0 +1,21 @@
+ import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import BT4 from './Compornent/BT4'
+// import BT5 from './Compornent/BT5'
+// import BT3 from './Compornent/BT3'
+// import BT2 from './Compornent/BT2'
+// import BT1 from './Compornent/BT1'
+
+function App() {
+  return (
+    <>
+      {/* <BT1/> */}
+      {/* <BT2/> */}
+     {/* <BT3/> */}
+     <BT4/>
+     {/* <BT5/> */}
+     </>
+  )
+}
+
+export default App
---
+++ ss21/src/Compornent/BT1.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BT1() {
+    return (
+        <div>
+            <h1 className="text-danger" style={{ fontSize: "32px" }}>
+                Quỷ Vương Bất Tỉnh 
+            </h1>
+        </div>
+    )
+}
+
+export default BT1
